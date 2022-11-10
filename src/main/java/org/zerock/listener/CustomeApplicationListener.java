b318aa8bed62131749a1af32ff57550a28d6c4bf
MS5463 <110441724+MS5463@users.noreply.github.com>
2022-11-10 11:20:12 +0900

    20221110

---
+++ src/main/java/org/zerock/listener/CustomeApplicationListener.java
@@ -0,0 +1,38 @@
+package org.zerock.listener;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Application Lifecycle Listener implementation class CustomeApplicationListener
+ *
+ */
+
+public class CustomeApplicationListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public CustomeApplicationListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent sce)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         // application 영역에 attribute를 추가
+    	// https://sduty-2022-08-08-ms5463.s3.ap-northeast-2.amazonaws.com/prj1/board
+    	ServletContext application = sce.getServletContext();
+    	application.setAttribute("imgUrl", "https://sduty-2022-08-08-ms5463.s3.ap-northeast-2.amazonaws.com/prj1/board");
+    }
+	
+}
